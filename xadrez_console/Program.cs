36457ab778ea6d1363b94322e0e76243380400b7
alloes <alex.lopes898@gmail.com>
2023-03-16 16:42:35 -0300

    Classe posicao criada

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p;
+
+            p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + p);
+        }
+    }
+}
