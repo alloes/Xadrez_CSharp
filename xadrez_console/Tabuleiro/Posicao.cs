0169d82d90221b7b2dd9e0bb52a508a86c199a7d
alloes <alex.lopes898@gmail.com>
2023-03-16 17:22:01 -0300

    começo implementação do tabuleiro

--- xadrez_console/Tabuleiro/Posicao.cs
+++ xadrez_console/Tabuleiro/Posicao.cs
@@ -5,6 +5,10 @@
         public int linha { get; set; }
         public int coluna { get; set; }
 
+        public Posicao()
+        {
+        }
+
         public Posicao(int linha, int coluna)
         {
             this.linha = linha;
