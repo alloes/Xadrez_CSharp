4aa36bab6175d4fc1c352b0a832393dd2da2302e
alloes <alex.lopes898@gmail.com>
2023-03-20 22:11:43 -0300

    melhorando o metodo imprimirTabuleiro

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -9,33 +9,33 @@ namespace xadrez_console
 
         static void Main(string[] args)
         {
-            //try
-            //{
-            //    Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
-            //    tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 2));
-            //    tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(2, 9));
+                tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 2));
+                tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Branca), new Posicao(2, 7));
 
-            //    Tela.imprimirTabuleiro(tabuleiro);
+                Tela.imprimirTabuleiro(tabuleiro);
 
-            //    Console.WriteLine();
-            //}
-            //catch (TabuleiroException e)
-            //{
-            //    Console.WriteLine(e.Message); ;
-            //}
+                Console.WriteLine();
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message); ;
+            }
 
-            PosicaoXadrez posicaoXadrez = new PosicaoXadrez('a', 1);
+            //PosicaoXadrez posicaoXadrez = new PosicaoXadrez('a', 1);
 
-            Console.WriteLine(posicaoXadrez);
+            //Console.WriteLine(posicaoXadrez);
 
-            Console.WriteLine(posicaoXadrez.toPosicao());
+            //Console.WriteLine(posicaoXadrez.toPosicao());
 
-            posicaoXadrez = new PosicaoXadrez('c', 4);
+            //posicaoXadrez = new PosicaoXadrez('c', 4);
 
-            Console.WriteLine(posicaoXadrez);
+            //Console.WriteLine(posicaoXadrez);
 
-            Console.WriteLine(posicaoXadrez.toPosicao());
+            //Console.WriteLine(posicaoXadrez.toPosicao());
 
         }
     }
